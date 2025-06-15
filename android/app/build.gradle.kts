501774b8ff3500118bfff855dc6a8a22ff02941a
daniilzirsew <daniilzirsew@gmail.com>
2025-06-16 01:22:27 +0400

    Прописана NDK версия

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.edu_track"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
