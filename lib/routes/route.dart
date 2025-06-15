93eec18e2884271f5007edac5a650799d2eb8863
daniilzirsew <daniilzirsew@gmail.com>
2025-06-16 01:05:56 +0400

    Добавлена заставка и лого

--- lib/routes/route.dart
+++ lib/routes/route.dart
@@ -1,8 +1,14 @@
 import 'package:go_router/go_router.dart';
 import 'package:edu_track/ui/screens/welcome_screen.dart';
+import 'package:edu_track/ui/screens/splash_screen.dart';
 
 final GoRouter router = GoRouter(
+  initialLocation: '/splash',
   routes: [
+    GoRoute(
+      path: '/splash',
+      builder: (context, state) => const SplashScreen(),
+    ),
     GoRoute(
       path: '/',
       builder: (context, state) => const WelcomeScreen(),
---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 5), () {
+      context.go('/');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              width: 120,
+              height: 120,
+            ),
+            const SizedBox(height: 20),
+            const Text(
+              'EduTrack',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(69, 49, 144, 1.0),
+                letterSpacing: 1.2,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
