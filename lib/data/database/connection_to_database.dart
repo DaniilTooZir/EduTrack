16d61c677bdeb9d4141c4e7b82e09587b39f4649
daniilzirsew <daniilzirsew@gmail.com>
2025-06-20 01:44:36 +0400

    Исправление бага, когда на декстопе не отправлялись запросы в базу данных

--- lib/data/database/connection_to_database.dart
+++ lib/data/database/connection_to_database.dart
@@ -1,5 +1,6 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:edu_track/data/database/clean_http_client.dart';
 // Класс для инициализации и доступа к Supabase
 class SupabaseConnection{
   // Инициализация Supabase с использованием переменных окружения
@@ -7,6 +8,7 @@ class SupabaseConnection{
     await Supabase.initialize(
       url: dotenv.env['SUPABASE_URL']!,
       anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+      httpClient: CleanHttpClient(),
     );
   }
   // Геттер для получения текущего экземпляра клиента Supabase
